db0c706555aae5d70ba4cb8b7a0908323e60ef2a
Tyler <tbuhay@academic.rrc.ca>
2025-11-17 15:54:59 -0600

    Stable snapshot before changes.

--- app/src/main/java/com/example/celestia/data/network/LunarApi.kt
+++ app/src/main/java/com/example/celestia/data/network/LunarApi.kt
@@ -6,7 +6,6 @@ import retrofit2.http.Query
 
 interface LunarApi {
 
-    // GET https://api.ipgeolocation.io/astronomy?apiKey=YOUR_KEY&lat=49.8951&long=-97.1384
     @GET("astronomy")
     suspend fun getLunarPhase(
         @Query("apiKey") apiKey: String,
